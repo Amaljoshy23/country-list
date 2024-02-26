c87b5bfa83eacc9b4166b99e0765d93d8dc9af36
Amaljoshy23 <148259363+Amaljoshy23@users.noreply.github.com>
2024-02-26 11:39:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Api from './new components/Apiconnection'
+function App() {
+  return (
+    <div className="App">
+      <Api/>
+      
+    </div>
+  );
+}
+
+export default App;
+
